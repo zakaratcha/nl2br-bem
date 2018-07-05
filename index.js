d41d645215de716c0a3f358160de8e21d8da48f5
zakaratcha <zakaratcha@ya.ru>
2018-07-05 22:29:59 +0300

    es5 & rename

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 (function () {
-    const nl2br = function (str) {
-        const br = {tag: 'br'};
+    var nl2brX = function (str) {
+        var br = {tag: 'br'};
 
         return str.split(/[(\r\n)|(\n\r)|\r|\n]+/g).map((subStr, i) => i ? [br, subStr] : subStr);
     };
 
     if (typeof module === 'object' && typeof module.exports === 'object') {
-        module.exports = nl2br;
+        module.exports = nl2brX;
     }
 
     if (typeof modules === 'object' && typeof modules.define === 'function') {
-        modules.define('nl2br', function (provide) {
-            provide(nl2br);
+        modules.define('nl2brX', function (provide) {
+            provide(nl2brX);
         });
     }
 })();
